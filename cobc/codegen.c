b5901118f3987c95c1837038d424d0629d6de874
Keisuke Nishida <knishida@netlab.jp>
2002-04-26 15:22:19 +0000

    *** empty log message ***

--- cobc/codegen.c
+++ cobc/codegen.c
@@ -1728,8 +1728,7 @@ codegen (struct program_spec *spec)
 
   output_line ("/* PROCEDURE DIVISION */");
   output_line ("lb_default_handler:");
-  output_line ("cob_runtime_error (\"File I-O error\");");
-  output_line ("goto l_exit;");
+  output_line ("cob_exit (le_default_handler);");
 
   for (l = spec->exec_list; l; l = l->next)
     output_tree (l->item);
