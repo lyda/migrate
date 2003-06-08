17804dc631cc5bb7089974ead15560a3215641c0
Keisuke Nishida <knishida@netlab.jp>
2003-06-08 02:52:58 +0000

    *** empty log message ***

--- tests/cobol85/report.pl
+++ tests/cobol85/report.pl
@@ -17,7 +17,7 @@
 # the Free Software Foundation, Inc., 59 Temple Place, Suite 330,
 # Boston, MA 02111-1307 USA
 
-my $compile = 'cobc -P -std=cobol85';
+my $compile = 'cobc -std=cobol85';
 my $compile_module = 'cobc -std=cobol85 -m';
 
 my $num_progs = 0;
