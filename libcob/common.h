81ea2202105e4366f07bf9f8dd4cf6f303dd263f
Keisuke Nishida <knishida@netlab.jp>
2003-08-21 13:39:07 +0000

    * common.h (cob_module): New member `flag_binary_print_full'. 	* termio.c (cob_display): Binary full print.

--- libcob/common.h
+++ libcob/common.h
@@ -112,6 +112,7 @@ typedef struct __cob_module {
   unsigned char numeric_separator;
   unsigned char *collating_sequence;
   enum cob_display_sign display_sign;
+  unsigned long flag_binary_print_full : 1;
   struct __cob_module *next;
 } cob_module;
 
