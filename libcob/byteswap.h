1ac0f5c85f915f55cfd546d27b77186cfa721143
Roger While <simrw@sim-basis.de>
2005-05-27 11:11:18 +0000

    Correct byteswap.h

--- libcob/byteswap.h
+++ libcob/byteswap.h
@@ -27,6 +27,18 @@
 
 #include <sys/types.h>
 
+#ifndef	__BIT_TYPES_DEFINED__
+typedef signed char int8_t;
+typedef unsigned char u_int8_t;
+typedef short int16_t;
+typedef unsigned short u_int16_t;
+typedef int int32_t;
+typedef unsigned int u_int32_t;
+typedef long long int64_t;
+typedef unsigned long long u_int64_t;
+typedef int32_t register_t;
+#endif
+
 /* Basic bit swapping functions
  */
 #define COB_BSWAP_16_CONSTANT(val)	((u_int16_t) (	\
