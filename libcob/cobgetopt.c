c37f771fc41c8d01358f3a8ab61861b49fc4a61d
Simon 'sf-mensch/human' Sobisch <simonsobisch@gnu.org>
2016-01-31 19:05:25 +0000

    cobgetopt.c (cob_getopt_long_long): Fix [bugs:#194] and warnings of msc code analysis (possible memory issues) - switched from alloca to cobc's internal memory handling (cobc_malloc and cobc_free) * configure.ac: removed AC_FUNC_ALLOCA (all Makefiles and config.h changed accordingly)
    git-svn-id: https://svn.code.sf.net/p/open-cobol/code/branches/gnu-cobol-2.0@746 ed166372-6744-4ac0-a67f-bb1ae9efa102

--- libcob/cobgetopt.c
+++ libcob/cobgetopt.c
@@ -20,8 +20,8 @@
    <http://www.gnu.org/licenses/>.  */
 
 /*
-   Copyright (C) 2010-2012 Free Software Foundation, Inc.
-   Modified for use in GnuCOBOL by Roger While
+   Copyright (C) 2010-2012, 2014-2016 Free Software Foundation, Inc.
+   Modified for use in GnuCOBOL by Roger While, Simon Sobisch
 */
 
 #include "config.h"
@@ -52,11 +52,11 @@
 #define NONOPTION_P (argv[cob_optind][0] != '-' || argv[cob_optind][1] == '\0')
 
 
-/* This version of `getopt' appears to the caller like standard Unix `getopt'
+/* This version of 'getopt' appears to the caller like standard Unix 'getopt'
    but it behaves differently for the user, since it allows the user
    to intersperse the options with the other arguments.
 
-   As `getopt' works, it permutes the elements of ARGV so that,
+   As 'getopt' works, it permutes the elements of ARGV so that,
    when it is done, all the options precede everything else.  Thus
    all application programs are extended to handle flexible argument order.
 
@@ -68,24 +68,24 @@
 
 #include "cobgetopt.h"
 
-/* For communication from `getopt' to the caller.
-   When `getopt' finds an option that takes an argument,
+/* For communication from 'getopt' to the caller.
+   When 'getopt' finds an option that takes an argument,
    the argument value is returned here.
-   Also, when `ordering' is RETURN_IN_ORDER,
+   Also, when 'ordering' is RETURN_IN_ORDER,
    each non-option ARGV-element is returned here.  */
 
 char *cob_optarg = NULL;
 
 /* Index in ARGV of the next element to be scanned.
    This is used for communication to and from the caller
-   and for communication between successive calls to `getopt'.
+   and for communication between successive calls to 'getopt'.
 
-   On entry to `getopt', zero means this is the first call; initialize.
+   On entry to 'getopt', zero means this is the first call; initialize.
 
-   When `getopt' returns -1, this is the index of the first of the
+   When 'getopt' returns -1, this is the index of the first of the
    non-option elements that the caller should itself scan.
 
-   Otherwise, `optind' communicates from one call to the next
+   Otherwise, 'optind' communicates from one call to the next
    how much of ARGV has been scanned so far.  */
 
 /* 1003.2 says this must be 1 before any call.  */
@@ -127,7 +127,7 @@ int cob_optopt = '?';
    stop option processing when the first non-option is seen.
    This is what Unix does.
    This mode of operation is selected by either setting the environment
-   variable POSIXLY_CORRECT, or using `+' as the first character
+   variable POSIXLY_CORRECT, or using '+' as the first character
    of the list of option characters.
 
    PERMUTE is the default.  We permute the contents of ARGV as we scan,
@@ -139,12 +139,12 @@ int cob_optopt = '?';
    to expect options and other ARGV-elements in any order and that care about
    the ordering of the two.  We describe each non-option ARGV-element
    as if it were the argument of an option with character code 1.
-   Using `-' as the first character of the list of option characters
+   Using '-' as the first character of the list of option characters
    selects this mode of operation.
 
-   The special argument `--' forces an end of option-scanning regardless
-   of the value of `ordering'.  In the case of RETURN_IN_ORDER, only
-   `--' can cause `getopt' to return -1 with `optind' != ARGC.  */
+   The special argument '--' forces an end of option-scanning regardless
+   of the value of 'ordering'.  In the case of RETURN_IN_ORDER, only
+   '--' can cause 'getopt' to return -1 with 'optind' != ARGC.  */
 
 static enum {
 	REQUIRE_ORDER, PERMUTE, RETURN_IN_ORDER
@@ -153,8 +153,8 @@ static enum {
 /* Handle permutation of arguments.  */
 
 /* Describe the part of ARGV that contains non-options that have
-   been skipped.  `first_nonopt' is the index in ARGV of the first of them;
-   `last_nonopt' is the index after the last of them.  */
+   been skipped.  'first_nonopt' is the index in ARGV of the first of them;
+   'last_nonopt' is the index after the last of them.  */
 
 static int first_nonopt;
 static int last_nonopt;
@@ -167,7 +167,7 @@ static int	seen_short = 0;
    The other is elements [last_nonopt,optind), which contains all
    the options processed since those non-options were skipped.
 
-   `first_nonopt' and `last_nonopt' are relocated so that they describe
+   'first_nonopt' and 'last_nonopt' are relocated so that they describe
    the new indices of the non-options in ARGV after they are moved.  */
 
 static void
@@ -266,48 +266,48 @@ static const char *_getopt_initialize (const char *optstring)
 
    If an element of ARGV starts with '-', and is not exactly "-" or "--",
    then it is an option element.  The characters of this element
-   (aside from the initial '-') are option characters.  If `getopt'
+   (aside from the initial '-') are option characters.  If 'getopt'
    is called repeatedly, it returns successively each of the option characters
    from each of the option elements.
 
-   If `getopt' finds another option character, it returns that character,
-   updating `optind' and `nextchar' so that the next call to `getopt' can
+   If 'getopt' finds another option character, it returns that character,
+   updating 'optind' and 'nextchar' so that the next call to 'getopt' can
    resume the scan with the following option character or ARGV-element.
 
-   If there are no more option characters, `getopt' returns -1.
-   Then `optind' is the index in ARGV of the first ARGV-element
+   If there are no more option characters, 'getopt' returns -1.
+   Then 'optind' is the index in ARGV of the first ARGV-element
    that is not an option.  (The ARGV-elements have been permuted
    so that those that are not options now come last.)
 
    OPTSTRING is a string containing the legitimate option characters.
    If an option character is seen that is not listed in OPTSTRING,
-   return '?' after printing an error message.  If you set `opterr' to
+   return '?' after printing an error message.  If you set 'opterr' to
    zero, the error message is suppressed but we still return '?'.
 
    If a char in OPTSTRING is followed by a colon, that means it wants an arg,
    so the following text in the same ARGV-element, or the text of the following
-   ARGV-element, is returned in `optarg'.  Two colons mean an option that
+   ARGV-element, is returned in 'optarg'.  Two colons mean an option that
    wants an optional arg; if there is text in the current ARGV-element,
-   it is returned in `optarg', otherwise `optarg' is set to zero.
+   it is returned in 'optarg', otherwise 'optarg' is set to zero.
 
-   If OPTSTRING starts with `-' or `+', it requests different methods of
+   If OPTSTRING starts with '-' or '+', it requests different methods of
    handling the non-option ARGV-elements.
    See the comments about RETURN_IN_ORDER and REQUIRE_ORDER, above.
 
-   Long-named options begin with `--' instead of `-'.
+   Long-named options begin with '--' instead of '-'.
    Their names may be abbreviated as long as the abbreviation is unique
    or is an exact match for some defined option.  If they have an
    argument, it follows the option name in the same ARGV-element, separated
-   from the option name by a `=', or else the in next ARGV-element.
-   When `getopt' finds a long-named option, it returns 0 if that option's
-   `flag' field is nonzero, the value of the option's `val' field
-   if the `flag' field is zero.
+   from the option name by a '=', or else the in next ARGV-element.
+   When 'getopt' finds a long-named option, it returns 0 if that option's
+   'flag' field is nonzero, the value of the option's 'val' field
+   if the 'flag' field is zero.
 
    The elements of ARGV aren't really const, because we permute them.
    But we pretend they're const in the prototype to be compatible
    with other systems.
 
-   LONGOPTS is a vector of `struct option' terminated by an
+   LONGOPTS is a vector of 'struct option' terminated by an
    element containing a name which is zero.
 
    LONGIND returns the index in LONGOPT of the long-named option found.
@@ -370,7 +370,7 @@ cob_getopt_long_long (const int argc, char *const *argv, const char *optstring,
 	  last_nonopt = cob_optind;
 	}
 
-      /* The special ARGV-element `--' means premature end of options.
+      /* The special ARGV-element '--' means premature end of options.
 	 Skip it like a null option,
 	 then exchange with previous non-options as if it were an option,
 	 then skip everything else like a non-option.  */
@@ -440,11 +440,11 @@ cob_getopt_long_long (const int argc, char *const *argv, const char *optstring,
       unsigned int namelen;
       const struct option *p;
       const struct option *pfound = NULL;
-      struct option_list
-      {
-	const struct option *p;
-	struct option_list *next;
+      struct option_list {
+		const struct option *p;
+		struct option_list *next;
       } *ambig_list = NULL;
+	  struct option_list *ambig_last = NULL;
       int exact = 0;
       int indfound = -1;
       int option_index;
@@ -456,39 +456,33 @@ cob_getopt_long_long (const int argc, char *const *argv, const char *optstring,
       /* Test all long options for either exact match
 	 or abbreviated matches.  */
       for (p = longopts, option_index = 0; p->name; p++, option_index++)
-	if (!strncmp (p->name, nextchar, namelen))
-	  {
-	    if (namelen == (unsigned int) strlen (p->name))
-	      {
-		/* Exact match found.  */
-		pfound = p;
-		indfound = option_index;
-		exact = 1;
-		break;
-	      }
-	    else if (pfound == NULL)
-	      {
-		/* First nonexact match found.  */
-		pfound = p;
-		indfound = option_index;
-	      }
-	    else if (long_only
+	if (!strncmp (p->name, nextchar, namelen)) {
+	    if (namelen == (unsigned int) strlen (p->name)) {
+			/* Exact match found.  */
+			pfound = p;
+			indfound = option_index;
+			exact = 1;
+			break;
+	    } else if (pfound == NULL) {
+			/* First nonexact match found.  */
+			pfound = p;
+			indfound = option_index;
+	    } else if (long_only
 		     || pfound->has_arg != p->has_arg
 		     || pfound->flag != p->flag
-		     || pfound->val != p->val)
-	      {
-		/* Second or later nonexact match found.  */
-		struct option_list *newp = alloca (sizeof (*newp));
-		newp->p = p;
-		newp->next = ambig_list;
-		ambig_list = newp;
-	      }
+		     || pfound->val != p->val) {
+			/* Second or later nonexact match found.  */
+			struct option_list *newp = cob_fast_malloc (sizeof (*newp));
+			newp->p = p;
+			newp->next = ambig_list;
+			ambig_list = newp;
+			ambig_last = ambig_list;
+	    }
 	  }
 
-      if (ambig_list != NULL && !exact)
-	{
-	  if (cob_opterr)
-	    {
+	  if (ambig_list != NULL){
+	   if (!exact) {
+	    if (cob_opterr) {
 	      struct option_list first;
 	      first.p = pfound;
 	      first.next = ambig_list;
@@ -497,23 +491,29 @@ cob_getopt_long_long (const int argc, char *const *argv, const char *optstring,
 	      fprintf (stderr, _("%s: option '%s' is ambiguous; possibilities:"),
 		       argv[0], argv[cob_optind]);
 
-	      do
-		{
-		  fprintf (stderr, " '--%s'", ambig_list->p->name);
-		  ambig_list = ambig_list->next;
-		}
+	      do {
+			  fprintf (stderr, " '--%s'", ambig_list->p->name);
+			  ambig_list = ambig_list->next;
+		  }
 	      while (ambig_list != NULL);
 
 	      fputc ('\n', stderr);
 	    }
+	 }
+ 	 while (ambig_last != NULL) {
+		ambig_list = ambig_last;
+		ambig_last = ambig_last->next;
+		cob_free (ambig_list);
+	 }
+	 if (!exact) {
 	  nextchar += strlen (nextchar);
 	  cob_optind++;
 	  cob_optopt = 0;
 	  return '?';
+	 }
 	}
 
-      if (pfound != NULL)
-	{
+    if (pfound != NULL) {
 	  option_index = indfound;
 	  cob_optind++;
 	  if (*nameend)
@@ -613,7 +613,7 @@ cob_getopt_long_long (const int argc, char *const *argv, const char *optstring,
     char c = *nextchar++;
     char *temp = strchr (optstring, c);
 
-    /* Increment `cob_optind' when we start to process its last character.  */
+    /* Increment 'cob_optind' when we start to process its last character.  */
     if (*nextchar == '\0')
     {
       ++cob_optind;
@@ -668,13 +668,13 @@ cob_getopt_long_long (const int argc, char *const *argv, const char *optstring,
 	    return c;
 	  }
 	else
-	  /* We already incremented `cob_optind' once;
+	  /* We already incremented 'cob_optind' once;
 	     increment it again when taking next ARGV-elt as argument.  */
 	  cob_optarg = argv[cob_optind++];
 
 	/* cob_optarg is now the argument, see if it's in the
 	   table of longopts.  */
-
+	if (!longopts) return '?'; /* silence warning */
 	for (nextchar = nameend = cob_optarg; *nameend && *nameend != '='; nameend++)
 	  /* Do nothing.  */ ;
 
@@ -745,7 +745,7 @@ cob_getopt_long_long (const int argc, char *const *argv, const char *optstring,
 		  {
 		    if (cob_opterr)
 		      {
-			fprintf (stderr, _("%s: option `%s' requires an argument"),
+			fprintf (stderr, _("%s: option '%s' requires an argument"),
 				 argv[0], argv[cob_optind - 1]);
 			fputc ('\n', stderr);
 		      }
@@ -808,7 +808,7 @@ cob_getopt_long_long (const int argc, char *const *argv, const char *optstring,
 		  c = '?';
 	      }
 	    else
-	      /* We already incremented `cob_optind' once;
+	      /* We already incremented 'cob_optind' once;
 		 increment it again when taking next ARGV-elt as argument.  */
 	      cob_optarg = argv[cob_optind++];
 	    nextchar = NULL;
