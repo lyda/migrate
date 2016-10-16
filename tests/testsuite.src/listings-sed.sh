893462e10e992916832284f785a0186d516a0861
Simon 'sf-mensch/human' Sobisch <simonsobisch@gnu.org>
2016-10-16 22:56:06 +0000

    testsuite changes for listing tests:
    * atlocal.in, atlocal_win: added UNIFY_LISTING
    * testsuite.src/listings.at: moved simplified sed commands used to a single script listings-sed.sh and use this via UNIFY_LISTING
    * testsuite.src/listings.at: split symbols test, added test for -tlines, use -tlines=0 to remove unforced page breaks
    
    additional:
    
    * atlocal_win: added missing COB_HAS_UTC_OFFSET
    
    git-svn-id: https://svn.code.sf.net/p/open-cobol/code/branches/gnu-cobol-2.0@1163 ed166372-6744-4ac0-a67f-bb1ae9efa102

---
+++ tests/testsuite.src/listings-sed.sh
@@ -0,0 +1,8 @@
+#! /bin/sh
+# Necessary sed replacements for unifying a listing
+
+sed \
+-e 's/GnuCOBOL [0-9].[0-9].[0-9]/GnuCOBOL V.R.P/g' \
+-e 's/[0-2][0-9]:[0-6][0-9]:[0-9][0-9] [0-9][0-9][0-9][0-9]/HH:MM:SS YYYY/g' \
+-e 's/'"`date +"%a %b %e"`"'/DDD MMM dd/g' \
+<"$1" >"$2"
