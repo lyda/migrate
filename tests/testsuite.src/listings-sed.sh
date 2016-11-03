47a2e71e4744025c9b5288e4d5a0f3b38d33839c
Simon 'sf-mensch/human' Sobisch <simonsobisch@gnu.org>
2016-11-03 22:51:44 +0000

    testsuite changes for listing tests:
    * testsuite.src/listings-sed.sh: changed to use basic regular expressions - fixing [bugs:#355]
    
    git-svn-id: https://svn.code.sf.net/p/open-cobol/code/branches/gnu-cobol-2.0@1208 ed166372-6744-4ac0-a67f-bb1ae9efa102

--- tests/testsuite.src/listings-sed.sh
+++ tests/testsuite.src/listings-sed.sh
@@ -2,7 +2,7 @@
 # Necessary sed replacements for unifying a listing
 
 sed \
--e 's/GnuCOBOL [0-9]\+.[0-9]\+.[0-9]\+ \+/GnuCOBOL V.R.P   /g' \
+-e 's/GnuCOBOL [0-9][0-9]*\.[0-9][0-9]*\.[0-9][0-9]*  */GnuCOBOL V.R.P   /g' \
 -e 's/[0-2][0-9]:[0-6][0-9]:[0-9][0-9] [0-9][0-9][0-9][0-9]/HH:MM:SS YYYY/g' \
 -e 's/'"`date +"%a %b %e"`"'/DDD MMM dd/g' \
 <"$1" >"$2"
