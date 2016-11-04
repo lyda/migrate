b34677aad7d4017ce2cd1750fb1229445789dbfc
Simon 'sf-mensch/human' Sobisch <simonsobisch@gnu.org>
2016-11-04 15:28:39 +0000

    testsuite changes: * atlocal_win: use ${abs_srcdir} to be able to source the file from everywhere * listings-sed.sh: adjustment for runtime libraries without support for strftime modifier %E * cobol85/Makefile.am: ensure a "true" return even if the module result is different (otherwise make aborts)
    git-svn-id: https://svn.code.sf.net/p/open-cobol/code/branches/gnu-cobol-2.0@1210 ed166372-6744-4ac0-a67f-bb1ae9efa102

--- tests/listings-sed.sh
+++ tests/listings-sed.sh
@@ -1,8 +1,12 @@
 #! /bin/sh
 # Necessary sed replacements for unifying a listing
 
+# Note: We replace the date two times, as not all systems have %e modifier in C
+#       and use %E in this case ("Mon Feb 04" instead of "Mon Feb  4").
+
 sed \
 -e 's/GnuCOBOL [0-9][0-9]*\.[0-9][0-9]*\.[0-9][0-9]*  */GnuCOBOL V.R.P   /g' \
 -e 's/[0-2][0-9]:[0-6][0-9]:[0-9][0-9] [0-9][0-9][0-9][0-9]/HH:MM:SS YYYY/g' \
 -e 's/'"$(date +"%a %b %e")"'/DDD MMM dd/g' \
+-e 's/'"$(date +"%a %b %d")"'/DDD MMM dd/g' \
 <"$1" >"$2"
