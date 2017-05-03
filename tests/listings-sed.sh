456021a92ecfcf4bdc7e9b17ba2e762d70a5405c
Simon 'sf-mensch/human' Sobisch <simonsobisch@gnu.org>
2017-05-03 12:35:34 +0000

    tests/listings-sed.sh: simplified (more likely to run in non GNU/Linux environments)
    git-svn-id: https://svn.code.sf.net/p/open-cobol/code/branches/gnu-cobol-2.0@1568 ed166372-6744-4ac0-a67f-bb1ae9efa102

--- tests/listings-sed.sh
+++ tests/listings-sed.sh
@@ -27,19 +27,22 @@
 # Note: We replace the date two times, as not all systems have %e modifier in C
 #       and use %E in this case ("Mon Feb 04" instead of "Mon Feb  4").
 
+date1=$(date +"%a %b %e")
+date2=$(date +"%a %b %d")
+
 if test "$3" = "once"; then
 	sed \
 	-e 's/GnuCOBOL [0-9][0-9]*\.[0-9][0-9]*[-devalphabetarc]*[0-9]*\.[0-9][0-9]*  */GnuCOBOL V.R.P               /g' \
 	-e 's/[0-2][0-9]:[0-6][0-9]:[0-9][0-9] [0-9][0-9][0-9][0-9]$/HH:MM:SS YYYY/g' \
-	-e 's/'"$(date +"%a %b %e")"'/DDD MMM dd/g' \
-	-e 's/'"$(date +"%a %b %d")"'/DDD MMM dd/g' \
+	-e 's/'"$date1"'/DDD MMM dd/g' \
+	-e 's/'"$date2"'/DDD MMM dd/g' \
 	<"$1" >"$2"
 else
 	sed \
 	-e 's/GnuCOBOL [0-9][0-9]*\.[0-9][0-9]*[-devalphabetarc]*[0-9]*\.[0-9][0-9]*  */GnuCOBOL V.R.P          /g' \
 	-e 's/[0-2][0-9]:[0-6][0-9]:[0-9][0-9] [0-9][0-9][0-9][0-9]/HH:MM:SS YYYY/g' \
-	-e 's/'"$(date +"%a %b %e")"'/DDD MMM dd/g' \
-	-e 's/'"$(date +"%a %b %d")"'/DDD MMM dd/g' \
+	-e 's/'"$date1"'/DDD MMM dd/g' \
+	-e 's/'"$date2"'/DDD MMM dd/g' \
 	<"$1" >"$2"
 fi
 
