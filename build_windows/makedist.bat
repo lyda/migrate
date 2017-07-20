a6078dfeb43770daf79a73449824bfe8f3536520
Simon 'sf-mensch/human' Sobisch <simonsobisch@gnu.org>
2017-07-20 12:00:28 +0000

    Merged revisions 1829-1831 from trunk:
    config:
    * general: allow GnuCOBOL specific registers for lax dialect configurations
    ........
    libcob:
    * common.h [__MINGW32__]: added missing defines
    ........
    build-windows:
    * makedist.bat: optimize during build of extras, improved displays to command line
    ........
    
    git-svn-id: https://svn.code.sf.net/p/open-cobol/code/branches/gnucobol-2.x@1832 ed166372-6744-4ac0-a67f-bb1ae9efa102

--- build_windows/makedist.bat
+++ build_windows/makedist.bat
@@ -49,129 +49,140 @@ if exist "%COB_RELEASE_PATH%x64\Release\cobc.exe" (
 )
 
 if "%HAVE32%"=="1" (
-   echo 32bit binaries found.
+   echo 32bit binaries: found
    if "%HAVE64%"=="1" (
-      echo 64bit binaries found.
+      echo 64bit binaries: found
    ) else (
-      echo No 64bit binaries found.
+      echo 64bit binaries: not found
    )
 ) else (
-   echo No 32bit binaries found.
+   echo 32bit binaries: not found
    if "%HAVE64%"=="1" (
-      echo 64bit binaries found.
+      echo 64bit binaries: found
    ) else (
-      echo No 64bit binaries found.
+      echo 64bit binaries: not found
       echo No binaries available, ABORT!
       goto :over
    )
 )
+echo.
 
 
-copy "%COB_SOURCE_PATH%AUTHORS"			.\AUTHORS.TXT
-copy "%COB_SOURCE_PATH%COPYING"			.\COPYING.TXT
-copy "%COB_SOURCE_PATH%COPYING.LESSER"	.\COPYING.LESSER.TXT
-copy "%COB_SOURCE_PATH%COPYING.DOC"		.\COPYING.DOC.TXT
-copy "%COB_SOURCE_PATH%NEWS"			.\NEWS.TXT
-copy "%COB_SOURCE_PATH%README"			.\README.TXT
-copy "%COB_SOURCE_PATH%THANKS"			.\THANKS.TXT
-copy "%COB_SOURCE_PATH%TODO"			.\TODO.TXT
+echo Copying docs...
+copy "%COB_SOURCE_PATH%AUTHORS"			.\AUTHORS.TXT	1>NUL
+copy "%COB_SOURCE_PATH%COPYING"			.\COPYING.TXT	1>NUL
+copy "%COB_SOURCE_PATH%COPYING.LESSER"	.\COPYING.LESSER.TXT	1>NUL
+copy "%COB_SOURCE_PATH%COPYING.DOC"		.\COPYING.DOC.TXT	1>NUL
+copy "%COB_SOURCE_PATH%NEWS"			.\NEWS.TXT		1>NUL
+copy "%COB_SOURCE_PATH%README"			.\README.TXT	1>NUL
+copy "%COB_SOURCE_PATH%THANKS"			.\THANKS.TXT	1>NUL
+copy "%COB_SOURCE_PATH%TODO"			.\TODO.TXT		1>NUL
+
+mkdir doc
+copy "%COB_SOURCE_PATH%doc\*.pdf"		doc\	1>NUL
+
+
+echo Copying configuration files...
+mkdir config
+copy "%COB_SOURCE_PATH%config\*.conf"		config\	1>NUL
+copy "%COB_SOURCE_PATH%config\*.conf-inc"	config\	1>NUL
+copy "%COB_SOURCE_PATH%config\*.words"		config\	1>NUL
+copy "%COB_SOURCE_PATH%config\*.cfg"		config\	1>NUL
+
+echo Copying copybooks...
+mkdir copy
+copy "%COB_SOURCE_PATH%copy\*.cpy"		copy\	1>NUL
+
+echo Copying header files...
+mkdir include
+mkdir include\libcob
+copy "%COB_SOURCE_PATH%libcob.h"		include\		1>NUL
+copy "%COB_SOURCE_PATH%libcob\common.h"		include\libcob\	1>NUL
+copy "%COB_SOURCE_PATH%libcob\exception.def"	include\libcob\	1>NUL
+copy "%COB_HEADER_PATH%gmp.h"			include\		1>NUL
+rem erase /S include\_*	1>NUL
+
+echo Copying translations...
+mkdir po
+for %%f in ("%COB_SOURCE_PATH%po\*.gmo") do (
+   copy "%%~ff"			po\%%~nf.mo	1>NUL
+)
+copy "%COB_SOURCE_PATH%po\*.po"			po\	1>NUL
+copy "%COB_SOURCE_PATH%po\*.pot"		po\	1>NUL
+erase /Q po\*@*	1>NUL
 
 if "%HAVE32%"=="1" (
-   copy "%COB_RELEASE_PATH%set_env_vs_dist.bat"	set_env_vs.bat
+   copy "%COB_RELEASE_PATH%set_env_vs_dist.bat"	set_env_vs.bat	1>NUL
    call :copyrel
 )
 if "%HAVE64%"=="1" (
-   copy "%COB_RELEASE_PATH%set_env_vs_dist_x64.bat"	set_env_vs_x64.bat
+   copy "%COB_RELEASE_PATH%set_env_vs_dist_x64.bat"	set_env_vs_x64.bat	1>NUL
    call :copyrel x64
 )
-
-
-mkdir config
-copy "%COB_SOURCE_PATH%config\*.conf"		config\
-copy "%COB_SOURCE_PATH%config\*.cfg"		config\
-
-mkdir copy
-copy "%COB_SOURCE_PATH%copy\*.cpy"		copy\
-
-mkdir doc
-copy "%COB_SOURCE_PATH%doc\*.pdf"		doc\
-
-mkdir include
-mkdir include\libcob
-copy "%COB_SOURCE_PATH%libcob.h"		include\
-copy "%COB_SOURCE_PATH%libcob\common.h"		include\libcob\
-copy "%COB_SOURCE_PATH%libcob\exception.def"	include\libcob\
-copy "%COB_HEADER_PATH%gmp.h"			include\
-rem erase /S include\_*
-
-mkdir po
-for %%f in ("%COB_SOURCE_PATH%po\*.gmo") do (
-   copy "%%~ff"			po\%%~nf.mo
-)
-copy "%COB_SOURCE_PATH%po\*.po"			po\
-copy "%COB_SOURCE_PATH%po\*.pot"		po\
-erase /Q po\*@*
 
 goto :end
 
 :copyrel
 if NOT "%1"=="x64" (
+   set mode=Win32
    set copyfrom="%COB_RELEASE_PATH%Win32\release"
    set copytobin=bin
    set copytolib=lib
 ) else (
-   set copyfrom="%COB_RELEASE_PATH%x64\release"
+   set mode=x64
    set copytobin=bin_x64
    set copytolib=lib_x64
 )
-mkdir %copytobin%
-copy "%copyfrom%\cobc.exe"			%copytobin%\
-copy "%copyfrom%\cobc.pdb"			%copytobin%\
-copy "%copyfrom%\cobcrun.exe"			%copytobin%\
-copy "%copyfrom%\cobcrun.pdb"			%copytobin%\
-copy "%copyfrom%\libcob.dll"			%copytobin%\
-copy "%copyfrom%\libcob.pdb"			%copytobin%\
+set copyfrom="%COB_RELEASE_PATH%%mode%\release"
 
-copy "%copyfrom%\libvbisam.dll"			%copytobin%\
-copy "%copyfrom%\mpir.dll"			%copytobin%\
-copy "%copyfrom%\pdcurses.dll"			%copytobin%\
+echo Copying binaries for %mode%...
+mkdir %copytobin%
+copy "%copyfrom%\cobc.exe"			%copytobin%\	1>NUL
+copy "%copyfrom%\cobc.pdb"			%copytobin%\	1>NUL
+copy "%copyfrom%\cobcrun.exe"			%copytobin%\	1>NUL
+copy "%copyfrom%\cobcrun.pdb"			%copytobin%\	1>NUL
+copy "%copyfrom%\libcob.dll"			%copytobin%\	1>NUL
+copy "%copyfrom%\libcob.pdb"			%copytobin%\	1>NUL
+
+copy "%copyfrom%\libvbisam.dll"			%copytobin%\	1>NUL
+copy "%copyfrom%\mpir.dll"			%copytobin%\	1>NUL
+copy "%copyfrom%\pdcurses.dll"			%copytobin%\	1>NUL
 
 mkdir %copytolib%"
-copy "%copyfrom%\libcob.lib"			%copytolib%\
+copy "%copyfrom%\libcob.lib"			%copytolib%\	1>NUL
 
 goto :eof
 
 :end
 :: must be last as we compile with the dist itself
+echo Copying extras...
 mkdir extras
-copy "%COB_SOURCE_PATH%extras\*.cob"		extras\
-copy "%COB_SOURCE_PATH%extras\README"		extras\README.txt
+copy "%COB_SOURCE_PATH%extras\*.cob"		extras\	1>NUL
+copy "%COB_SOURCE_PATH%extras\README"		extras\README.txt	1>NUL
 
 if "%HAVE32%"=="1" (
    echo.
-   echo.
-   echo Using created GnuCOBOL distribution -Win32- to compile extras
+   echo Using created GnuCOBOL distribution -Win32- to compile extras...
    cd "%COB_DIST_PATH%bin"
    call ..\set_env_vs.bat
-   cobc -m -Wall -std=mf ..\extras\CBL_OC_DUMP.cob -v
+   cobc -m -Wall -O2 ..\extras\CBL_OC_DUMP.cob -v
    cd ..
 )
 
 if "%HAVE64%"=="1" (
    echo.
-   echo.
-   echo Using created GnuCOBOL distribution -x64- to compile extras
+   echo Using created GnuCOBOL distribution -x64- to compile extras...
    cd "%COB_DIST_PATH%bin_x64"
    call ..\set_env_vs_x64.bat
-   cobc -m -Wall -std=mf ..\extras\CBL_OC_DUMP.cob -v
+   cobc -m -Wall -O2 ..\extras\CBL_OC_DUMP.cob -v
    cd ..
 )
 
 echo.
-echo.
 
+echo Compressing dist package...
 if exist "%ProgramFiles%\7-Zip\7z.exe" (
-   erase "..\GnuCOBOL.7z"
+   erase "..\GnuCOBOL.7z" 1>NUL
    "%ProgramFiles%\7-Zip\7z.exe" a -r -mx=9 "..\GnuCOBOL.7z" *
 ) else if exist "%ProgramFiles(x86)%\7-Zip\7z.exe" (
    erase "..\GnuCOBOL.7z"
