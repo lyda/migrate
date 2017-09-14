01b19adfb4f56f3b909474dca78a0b5f8ad79be6
Kevin Lyda <kevin@ie.suberic.net>
2017-09-14 20:40:35 +0100

    Make fetch work correctly.

--- COB-SVN2GIT.sh
+++ COB-SVN2GIT.sh
@@ -5,5 +5,5 @@ set -e
 if [[ ! -d gnucobol ]]; then
   git svn clone https://svn.code.sf.net/p/open-cobol/code --ignore-paths='^external-doc/' -A COB-SVN2GIT.DAT --stdlayout gnucobol
 else
-  cd gnucobol git svn fetch -A ../COB-SVN2GIT.DAT
+  cd gnucobol && git svn fetch -A ../COB-SVN2GIT.DAT
 fi
