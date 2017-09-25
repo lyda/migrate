47e81ad71fc0575161e997091f576e0fa9985fa6
Kevin Lyda <kevin@ie.suberic.net>
2017-09-25 18:04:39 +0100

    Add CVS import script (beginning).

---
+++ COB-CVS2GIT.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# See:
+# https://sourceforge.net/p/open-cobol/cvs/
+if [[ ! -d open-cobol ]]; then
+  mkdir open-cobol
+  cvs -d:pserver:anonymous@open-cobol.cvs.sourceforge.net:/cvsroot/open-cobol login
+  cvs -z3 -d:pserver:anonymous@open-cobol.cvs.sourceforge.net:/cvsroot/open-cobol co -P .
+fi
