bba8f210ce518e2138d12ecaadedc21871c956d9
Kevin Lyda <kevin@ie.suberic.net>
2017-09-26 22:01:44 +0100

    Add last cvs import command.

--- COB-CVS2GIT.sh
+++ COB-CVS2GIT.sh
@@ -6,4 +6,5 @@ if [[ ! -d open-cobol ]]; then
   mkdir open-cobol
   cvs -d:pserver:anonymous@open-cobol.cvs.sourceforge.net:/cvsroot/open-cobol login
   cvs -z3 -d:pserver:anonymous@open-cobol.cvs.sourceforge.net:/cvsroot/open-cobol co -P .
+  git cvsimport -C ../open-cobol.git open-cobol
 fi
